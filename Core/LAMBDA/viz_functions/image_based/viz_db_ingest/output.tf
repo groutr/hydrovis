384cff0873610e4b5e1a6596a71ff62320448a71
Ryan Grout <ryan.grout@noaa.gov>
2025-04-08 16:52:48 -0600

    Add terraform module for db-ingest.

---
+++ Core/LAMBDA/viz_functions/image_based/viz_db_ingest/output.tf
@@ -0,0 +1,3 @@
+output "db_ingest" {
+  value = data.aws_lambda_function.viz_lambda_function
+}
---
+++ Core/LAMBDA/viz_functions/image_based/viz_db_ingest/variables.tf
@@ -0,0 +1,48 @@
+variable "environment" {
+  type = string
+}
+
+variable "account_id" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
+variable "ecr_repository_image_tag" {
+  type = string
+  default = "latest"
+}
+
+variable "security_groups" {
+  type = list(string)
+}
+
+variable "subnets" {
+  type = list(string)
+}
+
+variable "deployment_bucket" {
+  type = string
+}
+
+variable "viz_db_name" {
+  type = string
+}
+
+variable "viz_db_host" {
+  type = string
+}
+
+variable "viz_db_user_secret_string" {
+  type = string
+}
+
+variable "lambda_role" {
+  type = string
+}
+
+variable "default_tags" {
+  type = map(string)
+}
